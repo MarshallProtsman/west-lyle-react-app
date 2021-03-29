d8f6ab9bbc86d97c6d96452dcaa8aad3ae6b18a7
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2021-03-29 07:10:03 -0400

    styled components initial implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Welcome from "../src/Components/Home/Home"
 
 function App() {
   return (
     <div className="App">
       <header >
+        <Welcome name="Marshall"/>
       </header>
     </div>
   );
